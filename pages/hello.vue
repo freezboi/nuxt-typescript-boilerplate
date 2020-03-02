55750c8014dd8461918503813fd5d25e1860c2eb
chihoon <chihoon@chihoonui-iMac.local>
2020-03-02 17:24:56 +0900

    demo project create

---
+++ pages/hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <logo />
+    <h1>Hello world!!</h1>
+    <p>{{ authorName }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+import Logo from '~/components/Logo.vue';
+
+@Component({
+  components: {
+    Logo
+  }
+})
+export default class HelloComponent extends Vue {
+  authorName: string = 'chihoon';
+}
+</script>
